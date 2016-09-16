a0e4b6f1108c677993e59f6d24844bfb1447f5cf
lnielsen <louiseenielsen@gmail.com>
2016-09-16 16:11:01 -0400

    one clickable text view

--- Lesson3_homework/app/src/main/java/nielsen/lesson3homework/MainActivityFragment.java
+++ Lesson3_homework/app/src/main/java/nielsen/lesson3homework/MainActivityFragment.java
@@ -1,12 +1,15 @@
 package nielsen.lesson3homework;
 
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -21,24 +24,37 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        Button myButton = (Button) view.findViewById(R.id.buttontest);
-        final TextView text = (TextView) view.findViewById(R.id.textView);
-        Button myButton2 = (Button) view.findViewById(R.id.button);
+        TextView myTextView1 = (TextView) view.findViewById(R.id.textView1);
+        TextView myTextView2 = (TextView) view.findViewById(R.id.textView2);
+        TextView myTextView3 = (TextView) view.findViewById(R.id.textView3);
+        TextView myTextView4 = (TextView) view.findViewById(R.id.textView4);
+        TextView myTextView5 = (TextView) view.findViewById(R.id.textView5);
 
-        myButton.setOnClickListener(new View.OnClickListener() {
+
+        myTextView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("MainActivityFragment", "Hello! I am a log");
-                text.setText("Goodbye earth!");
+//                TODO: add a text entry place and everything else after that (custom things)
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                Log.d("MainActivityFragment", "onClickListener got hit");
+                EditText edit_text = new EditText(v.getContext());
+                builder.setMessage(R.string.dialogMessage)
+                        .setView(edit_text)
+                        .setTitle(R.string.dialogTitle)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("MainActivityFragment", "User clicked OK");
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("MainActivityFragment", "User clicked Cancel");
+                            }
+                        });
+                builder.show();
             }
         });
 
-        myButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("MainActivityFragment2", "I am also a log");
-            }
-        });
 
         return view;
     }
