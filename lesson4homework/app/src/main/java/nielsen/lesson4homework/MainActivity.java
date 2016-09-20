6a35eeff635dd80fed96fde25f508569b79ca705
lnielsen <louiseenielsen@gmail.com>
2016-09-20 01:12:07 -0400

    can add toDos

--- lesson4homework/app/src/main/java/nielsen/lesson4homework/MainActivity.java
+++ lesson4homework/app/src/main/java/nielsen/lesson4homework/MainActivity.java
@@ -1,13 +1,19 @@
 package nielsen.lesson4homework;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,14 +21,37 @@ import butterknife.ButterKnife;
 //import layout.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.settingsButton) Button settingsButton;
+    private EditText editText;
+    private FloatingActionButton addButton;
+    private ListView listView;
+    private ArrayList<ToDo> toDos;
+    public ArrayAdapter<ToDo> toDoArrayAdapter;
+    private Button settingsButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        editText = (EditText) findViewById(R.id.editText);
+        listView = (ListView) findViewById(R.id.listView);
+        addButton = (FloatingActionButton) findViewById(R.id.addButton);
+        toDos = new ArrayList<>();
+        toDoArrayAdapter = new ToDoListAdapter(this, toDos);
+
+        listView.setAdapter(toDoArrayAdapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDos.add(new ToDo(editText.getText().toString(), false));
+                editText.setText("@string/editpls");
+                toDoArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
 
 
         final MainActivityFragment mainActivityFragment = new MainActivityFragment();
