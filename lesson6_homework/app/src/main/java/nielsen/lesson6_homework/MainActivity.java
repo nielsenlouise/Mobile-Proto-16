e686b73668d5cd268f86e37bfc3c619b2c1bd341
lnielsen <louiseenielsen@gmail.com>
2016-12-14 17:16:53 -0500

    it looks like a thing, but it's bad xml

--- lesson6_homework/app/src/main/java/nielsen/lesson6_homework/MainActivity.java
+++ lesson6_homework/app/src/main/java/nielsen/lesson6_homework/MainActivity.java
@@ -2,6 +2,9 @@ package nielsen.lesson6_homework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
